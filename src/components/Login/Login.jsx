82356dccd5953b68e9324b39d070740e001b3725
striverz <striverzarc@gmail.com>
2025-02-12 11:20:53 +0530

    created the login page and updated the font-styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Login.css"
+import logo from "../../assets/logo.png"
 
 const Login = () => {
+  const[isLogin,setIsLogin]=useState(1);
+  const handleLogin=()=>{
+    setIsLogin(!isLogin);
+  }
+
   return (
-    <div>Login</div>
+    <div className='login-page'>
+      <div className='logo-image'>
+        <img src={logo} alt='logo' className='logo'></img>
+      </div>
+      <form className='login-form'>
+        <h1>{isLogin ? "Sign in" : "Sign Up"}</h1>
+        {!isLogin && <input type="text" placeholder='Enter Your Name'></input>}
+        <input type="text" placeholder='Email Address'></input>
+        <input type='password' placeholder='password'></input>
+        <button>{isLogin ? "Sign In" :"Sign Up"}</button>
+        <p onClick={handleLogin}> { isLogin ? "New to Netflix? Sign up now." : "Already have Account? Sign in now"}</p>
+      </form>
+
+
+    </div>
   )
 }
 
