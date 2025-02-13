2b6693ad4dd60f46a0861c5e4e5cdbd0710109ba
striverz <striverzarc@gmail.com>
2025-02-13 09:17:12 +0530

    Threshold Point 0.1 🚀

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
     
 
         useEffect(()=>{
-          onAuthStateChanged(auth, (user) => {
+          const unsubscribe=onAuthStateChanged(auth, (user) => {
             if (user) {  
               const {uid:uid,email:email} = auth.currentUser;
               dispatch(addUser({uid:uid,email:email}));
@@ -47,6 +47,8 @@ const Header = () => {
              
             }
           });
+
+          return ()=>unsubscribe();
 
         },[]);
         
